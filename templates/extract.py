dac83ebca1d508ae9e5853b9712ebf2a00d5361f
chris-carlson <ccarlson9311@gmail.com>
2023-07-16 16:23:58 -0400

    Extract template parsing functions

--- templates/extract.py
+++ templates/extract.py
@@ -2,40 +2,14 @@ import dataclasses
 from typing import Dict, List
 
 from cac.io.json.json_writer import JsonWriter
-from cac.io.xml.xml_element import XmlElement
-from cac.io.xml.xml_reader import XmlReader
 from cac.path.directory import Directory
 from cac.path.file import File
 from cac.regex import Regex
 from ide import IDES, Ide, get_ide_subdirectory, has_ide
-from templates.template import Template, Variable
-from templates.value import convert_value
+from templates.template import Template, parse_templates
 
 TEMPLATE_FILE_REGEX: Regex = Regex(r'\.xml$')
 CONVERTED_TEMPLATE_DIRECTORY: Directory = Directory.get_cwd().join_directory('groups')
-
-SUBSTITUTIONS: Dict[str, str] = {'&#10;': '\\n', '    ': '\t', '&quot;': '\"', '&amp': '&', '&lt;': '<', '&gt;': '>'}
-
-def parse_templates(file: File) -> List[Template]:
-    reader: XmlReader = XmlReader(file.path)
-    template_set: XmlElement = reader.read_root()
-    return [parse_template(template) for template in template_set.children]
-
-def parse_template(template_element: XmlElement) -> Template:
-    name: str = template_element.attributes['name']
-    description: str = template_element.attributes['description']
-    value: str = template_element.attributes['value']
-    converted_value: str = convert_value(value, SUBSTITUTIONS)
-    variable_elements: List[XmlElement] = template_element.get_all_by_name('variable')
-    variables: List[Variable] = [parse_variable(variable_element) for variable_element in variable_elements]
-    return Template(name, description, converted_value, variables)
-
-def parse_variable(variable_element: XmlElement) -> Variable:
-    name: str = variable_element.attributes['name']
-    expression: str = variable_element.attributes['expression']
-    default_value: str = variable_element.attributes['defaultValue']
-    always_stop: bool = True if variable_element.attributes['alwaysStopAt'] == 'true' else False
-    return Variable(name, expression, default_value, always_stop)
 
 ides: List[Ide] = [ide for ide in IDES if has_ide(ide)]
 original_template_directories: List[Directory] = [get_ide_subdirectory(ide, 'templates') for ide in ides]
--- templates/template.py
+++ templates/template.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
-from typing import List
+from typing import Dict, List
+
+from cac.io.xml.xml_element import XmlElement
+from cac.io.xml.xml_reader import XmlReader
+from cac.path.file import File
+from templates.value import convert_value
 
 @dataclass
 class Template:
@@ -14,3 +19,26 @@ class Variable:
     expression: str
     default_value: str
     always_stop: bool
+
+SUBSTITUTIONS: Dict[str, str] = {'&#10;': '\\n', '    ': '\t', '&quot;': '\"', '&amp': '&', '&lt;': '<', '&gt;': '>'}
+
+def parse_templates(file: File) -> List[Template]:
+    reader: XmlReader = XmlReader(file.path)
+    template_set: XmlElement = reader.read_root()
+    return [parse_template(template) for template in template_set.children]
+
+def parse_template(template_element: XmlElement) -> Template:
+    name: str = template_element.attributes['name']
+    description: str = template_element.attributes['description']
+    value: str = template_element.attributes['value']
+    converted_value: str = convert_value(value, SUBSTITUTIONS)
+    variable_elements: List[XmlElement] = template_element.get_all_by_name('variable')
+    variables: List[Variable] = [parse_variable(variable_element) for variable_element in variable_elements]
+    return Template(name, description, converted_value, variables)
+
+def parse_variable(variable_element: XmlElement) -> Variable:
+    name: str = variable_element.attributes['name']
+    expression: str = variable_element.attributes['expression']
+    default_value: str = variable_element.attributes['defaultValue']
+    always_stop: bool = True if variable_element.attributes['alwaysStopAt'] == 'true' else False
+    return Variable(name, expression, default_value, always_stop)
